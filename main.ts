4ff0a68759526e8e28a2bea395b4549e8f9fc437
coraxites <coraxites@gmail.com>
2025-02-20 13:27:50 +0100

    logo and prompt

--- main.ts
+++ main.ts
@@ -1,1 +1,8 @@
-console.log("Hello World!");
+function start_up() {
+    console.log("  RENTZHOG     Capital")
+    console.log("CHRISTOPHERS   Management")
+    console.log("  WIRKKALA     Group LLC.")
+    console.log("                        & Sons")
+}
+
+start_up()
