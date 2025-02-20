19639cb9934c5a213d65d9facb1987872c977db3
Erik Rentzhog <erikuno1@hotmail.com>
2025-02-20 15:31:28 +0100

    Api fetching working

--- api.ts
+++ api.ts
@@ -1,9 +1,22 @@
-const API_KEY = "ycT2akQvJ7n6FDhD99q3oB6ypvqbYaBg"
+const apiKey: string = "ycT2akQvJ7n6FDhD99q3oB6ypvqbYaBg";
+const baseQuery: string = "https://api.polygon.io/v2/aggs/ticker"; 
+
+// What timespan to get from API
+const range: string = "1"
+const timespan: string = "minute"
+
+const simulationDate: string = "2025-01-30"
+
+let previousTime = Date.now();
 
 console.log(getResponse());
 
 function getResponse(){
-    const query: string = "https://api.polygon.io/v2/aggs/ticker/AAPL/range/1/day/2023-01-09/2023-01-09?apiKey=" + API_KEY; 
+    const from: string = simulationDate;
+    const to: string = simulationDate;
+    const ticker = "AAPL"; // Apple Inc.
+
+    const query: string = baseQuery + "/" + ticker + "/range/" + range + "/" + timespan + "/" + from + "/" + to + "?apiKey=" + apiKey;
 
     fetch(query)    
         .then(res => res.json())
@@ -11,10 +24,10 @@ function getResponse(){
         // The response has an `any` type, so we need to cast
         // it to the `User` type, and return it from the promise
 
-        if(res.status == "NOT_AUTHORIZED"){
+        if(res.status == "OK"){
+            return res
+        }else{
             throw new Error(res.message);
         }
-
-        return res
         })
 }
