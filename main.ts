f1b56e9a124d6356ab03fe0b710a84f1372530fc
coraxites <coraxites@gmail.com>
2025-02-20 13:28:36 +0100

    logo and prompt

--- main.ts
+++ main.ts
@@ -1,8 +1,12 @@
+// Initializes program
 function start_up() {
     console.log("  RENTZHOG     Capital")
     console.log("CHRISTOPHERS   Management")
     console.log("  WIRKKALA     Group LLC.")
     console.log("                        & Sons")
+
+    console.log("\nHow much money would you like to lose today? (SEK)")
+    const input: String = prompt("> ")
 }
 
 start_up()
