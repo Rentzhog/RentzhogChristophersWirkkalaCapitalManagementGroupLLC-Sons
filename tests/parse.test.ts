09ab3de418cea30d2e5685727f82d815bed58bd1
coraxites <coraxites@gmail.com>
2025-02-28 13:00:20 +0100

    cleaned up

--- tests/parse.test.ts
+++ tests/parse.test.ts
@@ -1,9 +1,5 @@
 import { json_to_timeline, timelines_to_market, timeline} from "../parse";
 
-// test('', () => {
-//     expect().toBe();
-// });
-
 test('json_to_timeline', () => {
     const json_string = '{"ticker":"AAPL","queryCount":3,"resultsCount":3,"adjusted":true,"results":[{"v":2628,"vw":247.117,"o":247.76,"c":246.81,"h":247.76,"l":246.81,"t":1740474000000,"n":89},{"v":1188,"vw":246.9877,"o":246.98,"c":247,"h":247,"l":246.98,"t":1740474060000,"n":56},{"v":952,"vw":247.0297,"o":247,"c":247,"h":247,"l":247,"t":1740474120000,"n":80}],"status":"OK","request_id":"e262aa5f0ad32808f97aa5b8d89dd9b7","count":770}'
     const timeline = json_to_timeline(json_string)
